94ad7cb6508f3f8e4317ef317e27eff638c88dad
Blessing Deyeye <blessingboluwatife7810@gmail.com>
2023-02-25 05:42:48 -0800

    Print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	putchar(x);
+	putchar('\n');
+	return (0);
+}
