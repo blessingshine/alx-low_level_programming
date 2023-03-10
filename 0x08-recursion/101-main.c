465fa77acadd209225501b52925b6af1191141b5
Blessing Deyeye <blessingboluwatife7810@gmail.com>
2023-03-10 12:01:42 -0800

    Recursion Project

--- 0x08-recursion/101-main.c
+++ 0x08-recursion/101-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     int r;
 
-    r = wildcmp("main.c", "*.c");
+    r = wildcmp("main.c", "*c");
     printf("%d\n", r);
     r = wildcmp("main.c", "m*a*i*n*.*c*");
     printf("%d\n", r);
